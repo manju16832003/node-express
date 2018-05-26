9d2c0f55adcba6c4f70428302eeea0b9f2b7374c
Manjunath Reddy <manju16832003@gmail.com>
2018-05-26 19:43:58 +0800

    Add post request

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const express = require('express')
 // get express object
 const app = express()
 
+app.use(express.json())
+
 // PORT environment variable. Read it from process
 const port = process.env.PORT || 3000
+
+const courses = [
+	{ id: 1, name: 'Course 1'},
+	{ id: 2, name: 'Course 2'},
+	{ id: 3, name: 'Course 3'},
+	{ id: 4, name: 'Course 4'}
+]
 
 app.get('/', (req, res) => {
 	res.send('Huurary......')
@@ -18,8 +27,38 @@ app.get('/api/courses', (req, res) => {
 // :id is route paramater
 // :id?sortBy=name is query string parameters
 app.get('/api/courses/:id', (req, res) => {
-	const id = req.params.id
-	res.send(id)
+	// Read route parameters
+	// res.send(req.params)
+
+	// read query string parameters
+	// res.send(req.query)
+
+	const course = courses.find( course => course.id === parseInt(req.params.id))
+
+	if(!course) {
+		res.status(404).send('The course with the given ID was not found')
+	}
+
+	res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+
+	// validate user input
+
+	if (!req.body.name) {
+		res.status(400).send('Name is required')
+		return; // NOTE: if not the script would continue to run and might get Error: Can't set headers after they are sent.
+	}
+
+	const course = {
+		id: courses.length + 1,
+		name: req.body.name
+	}
+
+	courses.push(course)
+
+	res.send(course)
 })
 
 app.listen(port, () => {
