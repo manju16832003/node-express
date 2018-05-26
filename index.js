774d64fca817217aabc00044abca42185ed7cdbf
Manjunath Reddy <manju16832003@gmail.com>
2018-05-26 20:23:25 +0800

    Upate resource with validation

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ app.get('/api/courses/:id', (req, res) => {
 	res.send(course)
 })
 
+// create new course
 app.post('/api/courses', (req, res) => {
 	// validate user input
 	const schema = {
@@ -52,7 +53,7 @@ app.post('/api/courses', (req, res) => {
 
 	if (result.error) {
 		res.status(400).send(result.error)
-		return; // NOTE: if not the script would continue to run and might get Error: Can't set headers after they are sent.
+		return // NOTE: if not the script would continue to run and might get Error: Can't set headers after they are sent.
 	}
 
 	const course = {
@@ -63,6 +64,36 @@ app.post('/api/courses', (req, res) => {
 	courses.push(course)
 
 	res.send(course)
+})
+
+// update existing course
+app.put('/api/courses/:id', (req, res) => {
+	// check if the course exists
+	const course = courses.find( course => course.id === parseInt(req.params.id))
+
+	if(!course) {
+		res.status(404).send('The course with the given ID was not found')
+	}
+
+	// validate the course
+	const schema = {
+		name: Joi.string().min(3).required()
+	}
+
+	const result = Joi.validate(req.body, schema)
+
+	if (result.error) {
+		res.status(400).send(result.error)
+		return // NOTE: if not the script would continue to run and might get Error: Can't set headers after they are sent.
+	}
+
+	// update the course
+	// find the index of the course
+	const courseIndex = courses.findIndex(course => course.id == req.params.id)
+	courses[courseIndex].name = req.body.name
+
+	// return the course
+	res.send(courses)
 })
 
 app.listen(port, () => {
